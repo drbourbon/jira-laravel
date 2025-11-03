261939a63c783ae5587e4111642eb68f28ced0d7
Fabio Barbon <fabio@neuma.studio>
2025-11-03 11:01:16 +0100

    Refactor Jira client authentication to use API key

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,6 +8,16 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Jira;
 use Jira\Client;
 use Jira\Laravel\Exceptions\ConfigIncomplete;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
+use Illuminate\Auth\Middleware\Authenticate;
+use App\Http\Middleware\BasicAuthenticateJiraMiddleware;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 /**
  * @internal
@@ -22,15 +32,16 @@ class ServiceProvider extends BaseServiceProvider
     public function register(): void
     {
         $this->app->singleton(Client::class, static function (): Client {
-            $username = config('jira.username');
-            $password = config('jira.password');
-            $host = config('jira.host');
 
-            if (! is_string($username) || ! is_string($password) || ! is_string($host)) {
+            $host = config('jira.host');
+            $user = Auth::user();
+            $apiKey = $user->jira_token;
+
+            if (! is_string($apiKey) || ! is_string($host)) {
                 throw ConfigIncomplete::create();
             }
 
-            return Jira::client($username, $password, $host);
+            return Jira::client($apiKey, $host);
         });
 
         $this->app->alias(Client::class, 'jira');
@@ -41,10 +52,14 @@ class ServiceProvider extends BaseServiceProvider
      *
      * @codeCoverageIgnore
      */
-    public function boot(): void
+    public function boot(Router $router): void
     {
         $this->publishes([
             __DIR__.'/../config/jira.php' => config_path('jira.php'),
+        ]);
+
+        $router->middlewareGroup('web', [
+            Authenticate::class
         ]);
     }
 
